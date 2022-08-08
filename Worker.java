525df7062431326eda95e213f0f7d99a2a86b83b
tarunsaisai <110806005+tarunsaisai@users.noreply.github.com>
2022-08-08 11:19:52 +0530

    Delete Worker.java

--- Worker.java
+++
@@ -1,8 +0,0 @@
-package com.java.support.JavaSupport.Service;
-
-import java.util.List;
-
-public interface Worker {
-
-	public List<com.java.support.JavaSupport.Model.EmployeeModel> getWorkerDetails();
-}
